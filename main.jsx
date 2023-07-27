61995570b17a14f83ac71ed209f9558320fb4f00
Gustavo Barbosa <124100829+gustavobs7@users.noreply.github.com>
2023-07-27 18:57:20 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Quadro from './Quadro/Quado.jsx'
+// import Agenda from './Agenda/Agenda.jsx'
+// import Feira from './Feira/Feira.jsx'
+// import Nomes from './Nomes/Nomes.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Quadro />
+    {/* <Agenda/> */}
+    {/* <Feira/> */}
+    {/* <Nomes/> */}
+  </React.StrictMode>,
+)
